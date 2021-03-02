1a9f0816d9c6ca93f35678ac2566d6d652a2528e
WebJohny <geryh213921@gmail.com>
2021-03-02 22:47:16 +0200

    master, added fields for parsing

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"linksparser/config"
 	"log"
 	//"os"
@@ -28,50 +27,50 @@ func main() {
 	// Run routes
 	//log.Fatal(MYSQL.GetFreeTask(0))
 
-	links := []string{
-		"https://exprealty.com/",
-		"https://join.exprealty.com/",
-		"https://buildingbetteragents.com/about-exp-realty/",
-		"https://www.linkedin.com/company/exp-realty",
-		"https://www.facebook.com/eXpRealty/",
-		"https://exprealty.ca/",
-		"https://www.globenewswire.com/news-release/2020/12/14/2144576/0/en/eXp-Realty-Surpasses-40-000-Real-Estate-Agents-Globally-On-Its-Immersive-Cloud-Based-Platform.html",
-		"https://www.indeed.com/cmp/Exp-Realty/reviews?fjobtitle=Realtor",
-		"https://expcloud.com/",
-		"https://www.inman.com/2018/05/30/i-toured-exp-realtys-virtual-reality-heres-what-its-like/",
-		"https://www.fortunebuilders.com/exp-realty-the-4-pillars-explained/",
-		"https://twitter.com/exprealty",
-		"https://www.expaustralia.com.au/",
-		"https://finance.yahoo.com/quote/EXPI/",
-		"https://exprealty.com/",
-		"https://join.exprealty.com/",
-		"https://buildingbetteragents.com/about-exp-realty/",
-		"https://www.linkedin.com/company/exp-realty",
-		"https://www.facebook.com/eXpRealty/",
-		"https://exprealty.ca/",
-		"https://www.globenewswire.com/news-release/2020/12/14/2144576/0/en/eXp-Realty-Surpasses-40-000-Real-Estate-Agents-Globally-On-Its-Immersive-Cloud-Based-Platform.html",
-		"https://www.indeed.com/cmp/Exp-Realty/reviews?fjobtitle=Realtor",
-		"https://expcloud.com/",
-		"https://www.inman.com/2018/05/30/i-toured-exp-realtys-virtual-reality-heres-what-its-like/",
-		"https://www.fortunebuilders.com/exp-realty-the-4-pillars-explained/",
-		"https://twitter.com/exprealty",
-		"https://www.expaustralia.com.au/",
-		"https://finance.yahoo.com/quote/EXPI/",
-		"https://exprealty.com/",
-		"https://join.exprealty.com/",
-		"https://buildingbetteragents.com/about-exp-realty/",
-		"https://www.linkedin.com/company/exp-realty",
-		"https://www.facebook.com/eXpRealty/",
-		"https://exprealty.ca/",
-		"https://www.globenewswire.com/news-release/2020/12/14/2144576/0/en/eXp-Realty-Surpasses-40-000-Real-Estate-Agents-Globally-On-Its-Immersive-Cloud-Based-Platform.html",
-		"https://www.indeed.com/cmp/Exp-Realty/reviews?fjobtitle=Realtor",
-		"https://expcloud.com/",
-		"https://www.inman.com/2018/05/30/i-toured-exp-realtys-virtual-reality-heres-what-its-like/",
-		"https://www.fortunebuilders.com/exp-realty-the-4-pillars-explained/",
-		"https://twitter.com/exprealty",
-		"https://www.expaustralia.com.au/",
-		"https://finance.yahoo.com/quote/EXPI/",
-	}
+	//links := []string{
+	//	"https://exprealty.com/",
+	//	"https://join.exprealty.com/",
+	//	"https://buildingbetteragents.com/about-exp-realty/",
+	//	"https://www.linkedin.com/company/exp-realty",
+	//	"https://www.facebook.com/eXpRealty/",
+	//	"https://exprealty.ca/",
+	//	"https://www.globenewswire.com/news-release/2020/12/14/2144576/0/en/eXp-Realty-Surpasses-40-000-Real-Estate-Agents-Globally-On-Its-Immersive-Cloud-Based-Platform.html",
+	//	"https://www.indeed.com/cmp/Exp-Realty/reviews?fjobtitle=Realtor",
+	//	"https://expcloud.com/",
+	//	"https://www.inman.com/2018/05/30/i-toured-exp-realtys-virtual-reality-heres-what-its-like/",
+	//	"https://www.fortunebuilders.com/exp-realty-the-4-pillars-explained/",
+	//	"https://twitter.com/exprealty",
+	//	"https://www.expaustralia.com.au/",
+	//	"https://finance.yahoo.com/quote/EXPI/",
+	//	"https://exprealty.com/",
+	//	"https://join.exprealty.com/",
+	//	"https://buildingbetteragents.com/about-exp-realty/",
+	//	"https://www.linkedin.com/company/exp-realty",
+	//	"https://www.facebook.com/eXpRealty/",
+	//	"https://exprealty.ca/",
+	//	"https://www.globenewswire.com/news-release/2020/12/14/2144576/0/en/eXp-Realty-Surpasses-40-000-Real-Estate-Agents-Globally-On-Its-Immersive-Cloud-Based-Platform.html",
+	//	"https://www.indeed.com/cmp/Exp-Realty/reviews?fjobtitle=Realtor",
+	//	"https://expcloud.com/",
+	//	"https://www.inman.com/2018/05/30/i-toured-exp-realtys-virtual-reality-heres-what-its-like/",
+	//	"https://www.fortunebuilders.com/exp-realty-the-4-pillars-explained/",
+	//	"https://twitter.com/exprealty",
+	//	"https://www.expaustralia.com.au/",
+	//	"https://finance.yahoo.com/quote/EXPI/",
+	//	"https://exprealty.com/",
+	//	"https://join.exprealty.com/",
+	//	"https://buildingbetteragents.com/about-exp-realty/",
+	//	"https://www.linkedin.com/company/exp-realty",
+	//	"https://www.facebook.com/eXpRealty/",
+	//	"https://exprealty.ca/",
+	//	"https://www.globenewswire.com/news-release/2020/12/14/2144576/0/en/eXp-Realty-Surpasses-40-000-Real-Estate-Agents-Globally-On-Its-Immersive-Cloud-Based-Platform.html",
+	//	"https://www.indeed.com/cmp/Exp-Realty/reviews?fjobtitle=Realtor",
+	//	"https://expcloud.com/",
+	//	"https://www.inman.com/2018/05/30/i-toured-exp-realtys-virtual-reality-heres-what-its-like/",
+	//	"https://www.fortunebuilders.com/exp-realty-the-4-pillars-explained/",
+	//	"https://twitter.com/exprealty",
+	//	"https://www.expaustralia.com.au/",
+	//	"https://finance.yahoo.com/quote/EXPI/",
+	//}
 
 	job := JobHandler{}
 	proxy := NewProxy()
@@ -81,27 +80,6 @@ func main() {
 	job.IsStart = true
 	job.Browser.Init()
 	job.proxy = *proxy
-	var results []SimilarWebResp
-	for i := 0; i < len(links); i++ {
-		link := links[i]
-		buf, err := job.Browser.ScreenShot(link)
-		if err != nil {
-			fmt.Println("ERR.JobHandler.Run.Screenshot", err)
-		}
-
-
-		//dsw, err := job.ExtractSimilarWebData(link)
-		//if err != nil {
-		//	fmt.Println("Iteration #" + strconv.Itoa(i), err)
-		//	continue
-		//}
-		//if dsw != nil {
-		//	results = append(results, *dsw)
-		//	time.Sleep(time.Second * time.Duration(rand.Intn(20)+3))
-		//}
-	}
-	log.Println(len(results))
-	log.Fatal(results)
 	job.IsStart = true
 	if job.Browser.Init() {
 		fmt.Println(job.Run(2))
