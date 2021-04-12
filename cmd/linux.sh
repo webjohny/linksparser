439af08ffd42cc636a6ad688d655c809b978af91
WebJohny <geryh213921@gmail.com>
2021-04-12 13:13:11 +0300

    master, fixd proxy

--- cmd/linux.sh
+++ cmd/linux.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # shellcheck disable=SC2034
-IP_ADDR="45.90.35.231"
+IP_ADDR="45.67.59.191"
 
 cd ../
 export GOOS=linux
 go build .
 export GOOS=windows
 
-#ssh "root@$IP_ADDR" "service linksparser stop"
-#scp -r linksparser "root@$IP_ADDR:/var/www/html"
-#ssh -i "~/.ssh/id_rsa" "root@$IP_ADDR" "service linksparser restart"
+ssh "root@$IP_ADDR" "service linksparser stop"
+scp -r linksparser "root@$IP_ADDR:/var/www/html"
+ssh -i "~/.ssh/id_rsa" "root@$IP_ADDR" "service linksparser restart"
 
 sleep 15
